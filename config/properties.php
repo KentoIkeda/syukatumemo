9559c7e70e24aee7f33f1fc4ef8d4f232161c60f
KentoIkeda <kento.windows10@gmail.com>
2020-11-05 05:38:18 +0900

    deleted

--- config/properties.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 接続データベース情報(本番)
-define('DATABASE_NAME','syukatsu');
-define('DATABASE_USER','root');
-define('DATABASE_PASSWORD','');
-define('DATABASE_HOST','localhost');
-
-define('PDO_DSN','mysql:dbname=' . DATABASE_NAME .';host=' . DATABASE_HOST . '; charset=utf8');
-?>
